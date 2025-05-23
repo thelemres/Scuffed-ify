753fa1c472d5819f6f8cc1c2c8b85ca23f8eb580
thelemres <jedmanaloto@hotmail.com>
2025-05-23 14:18:28 -0500

    Plan added, basics of Song included

---
+++ IPlayableAudio.java
@@ -0,0 +1,3 @@
+public interface IPlayableAudio {
+    public void playAudio();
+}
--- Song.java
+++ Song.java
@@ -1,7 +1,16 @@
-import javax.sound.sampled.*;
-import java.io.File;
-import java.io.IOException;
-
 public class Song {
-    
+    private String title;
+    private String artist;
+    private String audioFilePath;
+
+    public Song(String title, String artist, String audioFilePath) {
+        this.title = title;
+        this.artist = artist;
+        this.audioFilePath = audioFilePath;
+    }
+
+    @Override
+    public String toString() {
+        return title + " by " + artist;
+    }
 }
